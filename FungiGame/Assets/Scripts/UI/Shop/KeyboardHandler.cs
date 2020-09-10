523763186d27b5f56c62c2d78e7a76c15b35a26b
Jordan Dube <32602604+JordanDube@users.noreply.github.com>
2020-09-10 09:01:28 -0400

    Save system in place
    Need to call SaveGame() and LoadGame() in GameManager, but wait until the end to hook it up, as it messes with playtesting

---
+++ FungiGame/Assets/Scripts/UI/Shop/KeyboardHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyboardHandler : MonoBehaviour
+{
+    [SerializeField] private Text nameOutput;
+    private string outputText = "";
+    private int letterCounter;
+
+    public void AddLetter(char letter)
+    {
+        
+    }
+}
